3aa59c2d14b3a9fc999839784533d1d9af6e5aa5
Orlando Soto <jops0302@gmail.com>
2020-10-14 23:42:35 -0500

    revisando lo de los correos

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,5 +1,5 @@
 <?php
-// Check for empty fields
+// Compruebe si hay campos vacíos
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
@@ -10,12 +10,12 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-// Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+// Crea el correo electrónico y envía el mensaje
+$to = "jops0302@gmail.com"; // Agregue su dirección de correo electrónico entre "" reemplazando yourname@yourdomain.com - Aquí es donde el formulario enviará un mensaje.
+$subject = "Formulario de contacto del sitio web:  $name";
+$body = "Ha recibido un nuevo mensaje del formulario de contacto de su sitio web.\n\n"."Aquí están los details:\n\nNombre: $name\n\nEmail: $email\n\nTelefono: $phone\n\nMensaje:\n$message";
+$header = "de: noreply@yourdomain.com\n"; // Esta es la dirección de correo electrónico de la que procederá el mensaje generado. Recomendamos usar algo como noreply@yourdomain.com.
+$header .= "Responder a: $email";	
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
